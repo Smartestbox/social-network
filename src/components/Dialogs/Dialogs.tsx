1f52b3d0e22ac62faa0e5afbbf59873170e65f5f
Igor Olkhov <smartestbox@gmail.com>
2023-03-08 22:01:38 +0100

    Extend Dialogs component

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,10 +1,36 @@
 import React from 'react';
-import styles from 'Dialogs.module.css'
+import styles from './Dialogs.module.css'
 
 const Dialogs = () => {
     return (
         <div>
-           Dialogs
+           <div className={styles.dialogs}>
+               <div className={styles.dialogsItems}>
+                   <div className={`${styles.dialogItem} ${styles.active}`}>
+                       Dymich
+                   </div>
+                   <div className={styles.dialogItem}>
+                       Andrey
+                   </div>
+                   <div className={styles.dialogItem}>
+                       Sveta
+                   </div>
+                   <div className={styles.dialogItem}>
+                       Sasha
+                   </div>
+                   <div className={styles.dialogItem}>
+                       Viktor
+                   </div>
+                   <div className={styles.dialogItem}>
+                       Valera
+                   </div>
+               </div>
+               <div className={styles.messages}>
+                   <div className={`${styles.message} ${styles.active}`}>Hi</div>
+                   <div className={styles.message}>How is your day?</div>
+                   <div className={styles.message}>YO!</div>
+               </div>
+           </div>
         </div>
     );
 };
