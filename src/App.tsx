2977fafdcdeb8ec069056ac8371f8977ee77b243
Igor Olkhov <smartestbox@gmail.com>
2023-03-08 21:02:01 +0100

    Add routing to navbar

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,27 @@ import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import Dialogs from './components/Dialogs/Dialogs';
+import {BrowserRouter, Route} from "react-router-dom";
+import Music from "./components/Music/Music";
+import News from "./components/News/News";
+import Settings from "./components/Settings/Settings";
 
 function App() {
-  return (
-    <div className={styles.appWrapper}>
-        <Header />
-        <Navbar />
-        <div className={styles.appWrapperContent}>
-            <Dialogs />
-            {/*<Profile />*/}
-        </div>
-
-
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <div className={styles.appWrapper}>
+                <Header/>
+                <Navbar/>
+                <div className={styles.appWrapperContent}>
+                    <Route path={'/dialogs'} component={Dialogs} />
+                    <Route path={'/profile'} component={Profile} />
+                    <Route path={'/news'} component={News} />
+                    <Route path={'/music'} component={Music} />
+                    <Route path={'/settings'} component={Settings} />
+                </div>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,16 +1,38 @@
 import React from 'react';
 import styles from "./Navbar.module.css";
+import {NavLink} from "react-router-dom";
 
 const Navbar = () => {
     return (
         <nav className={styles.nav}>
-            <div className={styles.item}><a>Profile</a></div>
-            <div className={styles.item}><a>Messages</a></div>
-            <div className={styles.item}><a>News</a></div>
-            <div className={styles.item}><a>Music</a></div>
-            <div className={styles.item}><a>Settings</a></div>
+            <div className={styles.item}>
+                <NavLink to='/profile' activeClassName={styles.active}>
+                    Profile
+                </NavLink>
+            </div>
+            <div className={styles.item}>
+                <NavLink to='/dialogs' activeClassName={styles.active}>
+                    Messages
+                </NavLink>
+            </div>
+            <div className={styles.item}>
+                <NavLink to='/news' activeClassName={styles.active}>
+                    News
+                </NavLink>
+            </div>
+            <div className={styles.item}>
+                <NavLink to='/music' activeClassName={styles.active}>
+                    Music
+                </NavLink>
+            </div>
+            <div className={styles.item}>
+                <NavLink to='/settings' activeClassName={styles.active}>
+                    Settings
+                </NavLink>
+            </div>
         </nav>
     );
 };
 
 export default Navbar;
+
